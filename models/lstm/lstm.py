b9e97adc175c203d2a47afcd9a61995356a4646d
Yuan Yao <yaoyuannnn@gmail.com>
2020-04-19 22:25:22 -0400

    Add an LSTM model.

---
+++ models/lstm/lstm.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Python code for creating the Minerva network.
+#
+
+import sys
+sys.path.append('../../../nnet_lib/src/python/')
+import numpy as np
+from graph import *
+from tensor import *
+from ops import *
+from types_pb2 import *
+from recurrent import *
+
+def generate_random_data(shape):
+  r = np.random.RandomState(1234)
+  return (r.rand(*shape) * 0.005).astype(np.float32)
+
+def create_lstm_model():
+  with Graph(name="lstm_ref", backend="Reference") as graph:
+    input_tensor = Tensor(
+        data_layout=NTC, tensor_data=generate_random_data((1, 4, 32)))
+    # Tensors and kernels are initialized as NC layout.
+    # Layer 1 of LSTM.
+    wf0_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
+        (32, 64)))
+    wi0_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
+        (32, 64)))
+    wc0_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
+        (32, 64)))
+    wo0_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
+        (32, 64)))
+    # Layer 2 of LSTM.
+    wf1_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
+        (32, 64)))
+    wi1_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
+        (32, 64)))
+    wc1_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
+        (32, 64)))
+    wo1_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
+        (32, 64)))
+
+    # Inputs specified in shape (batch, timestep, size)
+    inputs = input_data(input_tensor, name="input")
+    lstm_layer0 = LSTM([wf0_tensor, wi0_tensor, wc0_tensor, wo0_tensor],
+                       name="lstm0")
+    lstm_layer1 = LSTM([wf1_tensor, wi1_tensor, wc1_tensor, wo1_tensor],
+                       name="lstm1")
+    outputs, state = lstm_layer0(inputs)
+    outputs, state = lstm_layer1(outputs)
+    return graph
+
+if __name__ != "main":
+  graph = create_lstm_model()
+  graph.print_summary()
+  graph.write_graph()
