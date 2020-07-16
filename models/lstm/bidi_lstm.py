8c9fca4b3c7b4b3106e31867e2fb5c88ed53e782
Yuan Yao <yaoyuannnn@gmail.com>
2020-07-16 12:17:56 -0400

    Update LSTM and Bi-LSTM models.

--- models/lstm/bidi_lstm.py
+++ models/lstm/bidi_lstm.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import sys
 import numpy as np
 
 from smaug.core import types_pb2
@@ -11,30 +10,29 @@ from smaug.python.ops import recurrent
 
 def generate_random_data(shape):
   r = np.random.RandomState(1234)
-  return (r.rand(*shape) * 0.005).astype(np.float16)
+  return (r.rand(*shape) * 0.005).astype(np.float32)
 
 def create_lstm_model():
-  with Graph(name="lstm_smv", backend="SMV") as graph:
+  with Graph(name="bidi_lstm_ref", backend="Reference") as graph:
     input_tensor = Tensor(
         data_layout=types_pb2.NTC, tensor_data=generate_random_data((1, 4, 32)))
     # Tensors and kernels are initialized as NC layout.
-    # Layer 1 of LSTM.
-    w0 = Tensor(
+    # Weights of forward LSTM.
+    w_f = Tensor(
         data_layout=types_pb2.NC, tensor_data=generate_random_data((128, 32)))
-    u0 = Tensor(
+    u_f = Tensor(
         data_layout=types_pb2.NC, tensor_data=generate_random_data((128, 32)))
-    # Layer 2 of LSTM.
-    w1 = Tensor(
+    # Weights of backward LSTM.
+    w_b = Tensor(
         data_layout=types_pb2.NC, tensor_data=generate_random_data((128, 32)))
-    u1 = Tensor(
+    u_b = Tensor(
         data_layout=types_pb2.NC, tensor_data=generate_random_data((128, 32)))
 
     # Inputs specified in shape (batch, timestep, size)
     inputs = data_op.input_data(input_tensor, name="input")
-    lstm_layer0 = recurrent.LSTM([w0, u0], name="lstm0")
-    lstm_layer1 = recurrent.LSTM([w1, u1], name="lstm1")
-    outputs, state = lstm_layer0(inputs)
-    outputs, state = lstm_layer1(outputs)
+    bidi_lstm = recurrent.BidirectionalLSTM([w_f, u_f], [w_b, u_b],
+                                            name="bidi_lstm")
+    outputs, state_fwd, state_bwd = bidi_lstm(inputs)
     return graph
 
 if __name__ != "main":
