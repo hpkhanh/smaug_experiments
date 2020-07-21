9e005a3da9ae6f5d38f8d2bdf3d7dfe576a4706b
Yuan Yao <yaoyuannnn@gmail.com>
2020-07-21 18:54:47 -0400

    Update Minerva model.

--- models/minerva/minerva_network.py
+++ models/minerva/minerva_network.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python
-#
-# Python code for creating the Minerva network.
-#
 
-import sys
-sys.path.append('../../../nnet_lib/src/python/')
+"""Create the Minerva network."""
+
 import numpy as np
-from graph import *
-from tensor import *
-from ops import *
-from types_pb2 import *
+
+from smaug.core import types_pb2
+from smaug.python.graph import Graph
+from smaug.python.tensor import Tensor
+from smaug.python.ops import data_op
+from smaug.python.ops import nn_ops
 
 def generate_random_data(shape):
   r = np.random.RandomState(1234)
-  return (r.rand(*shape) * 0.005).astype(np.float16)
+  return (r.rand(*shape) * 0.005).astype(np.float32)
 
 def create_minerva_model():
-  with Graph(name="minerva_smv", backend="SMV") as graph:
+  with Graph(name="minerva_ref", backend="Reference") as graph:
     # Tensors and kernels are initialized as NCHW layout.
     input_tensor = Tensor(
-        data_layout=NHWC,
-        tensor_data=generate_random_data((1, 28, 28, 1)))
-    fc0_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
-        (256, 784)))
-    fc1_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
-        (256, 256)))
-    fc2_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
-        (256, 256)))
-    fc3_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data(
-        (10, 256)))
+        data_layout=types_pb2.NHWC, tensor_data=generate_random_data(
+            (1, 28, 28, 1)))
+    fc0_tensor = Tensor(
+        data_layout=types_pb2.NC, tensor_data=generate_random_data((256, 784)))
+    fc1_tensor = Tensor(
+        data_layout=types_pb2.NC, tensor_data=generate_random_data((256, 256)))
+    fc2_tensor = Tensor(
+        data_layout=types_pb2.NC, tensor_data=generate_random_data((256, 256)))
+    fc3_tensor = Tensor(
+        data_layout=types_pb2.NC, tensor_data=generate_random_data((10, 256)))
 
-    act = input_data(input_tensor, "input")
-    act = mat_mul(act, fc0_tensor, activation=ReLU, name="fc0")
-    act = mat_mul(act, fc1_tensor, activation=ReLU, name="fc1")
-    act = mat_mul(act, fc2_tensor, activation=ReLU, name="fc2")
-    act = mat_mul(act, fc3_tensor, name="fc3")
+    act = data_op.input_data(input_tensor, "input")
+    act = nn_ops.mat_mul(act, fc0_tensor, activation=types_pb2.ReLU, name="fc0")
+    act = nn_ops.mat_mul(act, fc1_tensor, activation=types_pb2.ReLU, name="fc1")
+    act = nn_ops.mat_mul(act, fc2_tensor, activation=types_pb2.ReLU, name="fc2")
+    act = nn_ops.mat_mul(act, fc3_tensor, name="fc3")
     return graph
 
 if __name__ != "main":
