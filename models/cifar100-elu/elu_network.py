679404cdfcda35f5dd2d1487657041172db521c1
Yuan Yao <yaoyuannnn@gmail.com>
2020-07-21 20:59:17 -0400

    Update ELU model.

--- models/cifar100-elu/elu_network.py
+++ models/cifar100-elu/elu_network.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
-#
-# Examples for creating the CIFAR100-ELU network.
-#
+
+"""Example for creating the CIFAR100-ELU network."""
 
 import sys
-sys.path.append('../../../nnet_lib/src/python/')
 import numpy as np
-from graph import *
-from tensor import *
-from ops import *
-from types_pb2 import *
+
+from smaug.core.types_pb2 import *
+from smaug.python.graph import Graph
+from smaug.python.tensor import Tensor
+from smaug.python.ops import data_op
+from smaug.python.ops import nn_ops
 
 def generate_random_data(shape):
   r = np.random.RandomState(1234)
-  return (r.rand(*shape) * 0.005).astype(np.float32)
+  return (r.rand(*shape) * 0.005).astype(np.float16)
 
 def create_elu_model():
-  with Graph(name="elu_ref", backend="Reference", mem_policy=AllDma) as graph:
+  with Graph(name="elu_smv", backend="SMV", mem_policy=AllDma) as graph:
     # Tensors and kernels are initialized as NCHW layout.
     input_tensor = Tensor(
-        data_layout=NHWC,
-        tensor_data=generate_random_data((1, 32, 32, 3)))
+        data_layout=NHWC, tensor_data=generate_random_data((1, 32, 32, 3)))
     conv0_stack0_tensor = Tensor(
-        data_layout=NHWC,
-        tensor_data=generate_random_data((192, 5, 5, 3)))
+        data_layout=NHWC, tensor_data=generate_random_data((192, 5, 5, 3)))
     conv0_stack1_tensor = Tensor(
         data_layout=NHWC, tensor_data=generate_random_data((192, 1, 1, 192)))
     conv1_stack1_tensor = Tensor(
@@ -45,43 +43,48 @@ def create_elu_model():
     conv0_stack6_tensor = Tensor(
         data_layout=NHWC, tensor_data=generate_random_data((100, 1, 1, 300)))
 
-    act = input_data(input_tensor, name="input")
-    act = convolution(
+    act = data_op.input_data(input_tensor, name="input")
+    act = nn_ops.convolution(
         act, conv0_stack0_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv0_stack0")
-    act = max_pool(act, pool_size=[2, 2], stride=[2, 2], name="pool_stack0")
-    act = convolution(
+    act = nn_ops.max_pool(
+        act, pool_size=[2, 2], stride=[2, 2], name="pool_stack0")
+    act = nn_ops.convolution(
         act, conv0_stack1_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv0_stack1")
-    act = convolution(
+    act = nn_ops.convolution(
         act, conv1_stack1_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv1_stack1")
-    act = max_pool(act, pool_size=[2, 2], stride=[2, 2], name="pool_stack1")
-    act = convolution(
+    act = nn_ops.max_pool(
+        act, pool_size=[2, 2], stride=[2, 2], name="pool_stack1")
+    act = nn_ops.convolution(
         act, conv0_stack2_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv0_stack2")
-    act = convolution(
+    act = nn_ops.convolution(
         act, conv1_stack2_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv1_stack2")
-    act = max_pool(act, pool_size=[2, 2], stride=[2, 2], name="pool_stack2")
-    act = convolution(
+    act = nn_ops.max_pool(
+        act, pool_size=[2, 2], stride=[2, 2], name="pool_stack2")
+    act = nn_ops.convolution(
         act, conv0_stack3_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv0_stack3")
-    act = convolution(
+    act = nn_ops.convolution(
         act, conv1_stack3_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv1_stack3")
-    act = max_pool(act, pool_size=[2, 2], stride=[2, 2], name="pool_stack3")
-    act = convolution(
+    act = nn_ops.max_pool(
+        act, pool_size=[2, 2], stride=[2, 2], name="pool_stack3")
+    act = nn_ops.convolution(
         act, conv0_stack4_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv0_stack4")
-    act = convolution(
+    act = nn_ops.convolution(
         act, conv1_stack4_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv0_stack4")
-    act = max_pool(act, pool_size=[2, 2], stride=[2, 2], name="pool_stack4")
-    act = convolution(
+    act = nn_ops.max_pool(
+        act, pool_size=[2, 2], stride=[2, 2], name="pool_stack4")
+    act = nn_ops.convolution(
         act, conv0_stack5_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv0_stack5")
-    act = convolution(
+    act = nn_ops.convolution(
         act, conv0_stack6_tensor, stride=[1, 1], padding="same", activation=ELU,
         name="conv0_stack6")
     return graph
