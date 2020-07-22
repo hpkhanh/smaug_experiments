1c16b98ca9f3cca4ef0cdd0bd1ff728795ed8530
Yuan Yao <yaoyuannnn@gmail.com>
2020-07-21 21:01:45 -0400

    Update CIFAR10-CNN model.

--- models/cifar10-cnn/cnn_network.py
+++ models/cifar10-cnn/cnn_network.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-#
-# Python code for creating the LeNet5 network.
-#
+
+"""Example for creating the CIFAR10-CNN network."""
 
 import sys
-sys.path.append('../../../nnet_lib/src/python/')
 import numpy as np
-from graph import *
-from tensor import *
-from ops import *
-from types_pb2 import *
+
+from smaug.core.types_pb2 import *
+from smaug.python.graph import Graph
+from smaug.python.tensor import Tensor
+from smaug.python.ops import data_op
+from smaug.python.ops import nn_ops
 
 def generate_random_data(shape):
   r = np.random.RandomState(1234)
-  return (r.rand(*shape) * 0.005).astype(np.float32)
+  return (r.rand(*shape) * 0.005).astype(np.float16)
 
 def create_cnn_model():
-  with Graph(name="cnn_ref", backend="Reference", mem_policy=AllDma) as graph:
+  with Graph(name="cnn_smv", backend="SMV", mem_policy=AllDma) as graph:
     input_tensor = Tensor(data_layout=NHWC, tensor_data=generate_random_data(
         (1, 32, 32, 3)))
     conv0_tensor = Tensor(data_layout=NHWC, tensor_data=generate_random_data(
@@ -56,25 +56,25 @@ def create_cnn_model():
     fc1_tensor = Tensor(data_layout=NC, tensor_data=generate_random_data((10,
                                                                           512)))
 
-    act = input_data(input_tensor)
-    act = convolution(
+    act = data_op.input_data(input_tensor)
+    act = nn_ops.convolution(
         act, conv0_tensor, stride=[1, 1], padding="same", activation=ReLU)
-    act = batch_norm(
+    act = nn_ops.batch_norm(
         act, bn0_mean_tensor, bn0_var_tensor, bn0_gamma_tensor, bn0_beta_tensor)
-    act = convolution(
+    act = nn_ops.convolution(
         act, conv1_tensor, stride=[1, 1], padding="same", activation=ReLU)
-    act = max_pool(act, pool_size=[2, 2], stride=[2, 2])
-    act = batch_norm(
+    act = nn_ops.max_pool(act, pool_size=[2, 2], stride=[2, 2])
+    act = nn_ops.batch_norm(
         act, bn1_mean_tensor, bn1_var_tensor, bn1_gamma_tensor, bn1_beta_tensor)
-    act = convolution(
+    act = nn_ops.convolution(
         act, conv2_tensor, stride=[1, 1], padding="same", activation=ReLU)
-    act = convolution(
+    act = nn_ops.convolution(
         act, conv3_tensor, stride=[1, 1], padding="same", activation=ReLU)
-    act = max_pool(act, pool_size=[2, 2], stride=[2, 2])
-    act = batch_norm(
+    act = nn_ops.max_pool(act, pool_size=[2, 2], stride=[2, 2])
+    act = nn_ops.batch_norm(
         act, bn2_mean_tensor, bn2_var_tensor, bn2_gamma_tensor, bn2_beta_tensor)
-    act = mat_mul(act, fc0_tensor, activation=ReLU)
-    act = mat_mul(act, fc1_tensor)
+    act = nn_ops.mat_mul(act, fc0_tensor, activation=ReLU)
+    act = nn_ops.mat_mul(act, fc1_tensor)
     return graph
 
 if __name__ != "main":
