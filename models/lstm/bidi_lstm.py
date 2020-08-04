d2a244d479f9d30474db30a4c0151b0e5707ca0e
Yuan Yao <yaoyuannnn@gmail.com>
2020-08-04 10:24:40 -0400

    Update Bidirectional LSTM model.

--- models/lstm/bidi_lstm.py
+++ models/lstm/bidi_lstm.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python
 
 import numpy as np
-
-from smaug.core import types_pb2
-from smaug.python.graph import Graph
-from smaug.python.tensor import Tensor
-from smaug.python.ops import data_op
-from smaug.python.ops import recurrent
+import smaug as sg
 
 def generate_random_data(shape):
   r = np.random.RandomState(1234)
-  return (r.rand(*shape) * 0.005).astype(np.float32)
+  return (r.rand(*shape) * 0.005).astype(np.float16)
 
 def create_lstm_model():
-  with Graph(name="bidi_lstm_ref", backend="Reference") as graph:
-    input_tensor = Tensor(
-        data_layout=types_pb2.NTC, tensor_data=generate_random_data((1, 4, 32)))
-    # Tensors and kernels are initialized as NC layout.
+  with sg.Graph(name="bidi_lstm_smv", backend="SMV") as graph:
+    input_tensor = sg.Tensor(
+        data_layout=sg.NTC, tensor_data=generate_random_data((1, 4, 32)))
+    # sg.Tensors and kernels are initialized as NC layout.
     # Weights of forward LSTM.
-    w_f = Tensor(
-        data_layout=types_pb2.NC, tensor_data=generate_random_data((128, 32)))
-    u_f = Tensor(
-        data_layout=types_pb2.NC, tensor_data=generate_random_data((128, 32)))
+    w_f = sg.Tensor(
+        data_layout=sg.NC, tensor_data=generate_random_data((128, 32)))
+    u_f = sg.Tensor(
+        data_layout=sg.NC, tensor_data=generate_random_data((128, 32)))
     # Weights of backward LSTM.
-    w_b = Tensor(
-        data_layout=types_pb2.NC, tensor_data=generate_random_data((128, 32)))
-    u_b = Tensor(
-        data_layout=types_pb2.NC, tensor_data=generate_random_data((128, 32)))
+    w_b = sg.Tensor(
+        data_layout=sg.NC, tensor_data=generate_random_data((128, 32)))
+    u_b = sg.Tensor(
+        data_layout=sg.NC, tensor_data=generate_random_data((128, 32)))
 
     # Inputs specified in shape (batch, timestep, size)
-    inputs = data_op.input_data(input_tensor, name="input")
-    bidi_lstm = recurrent.BidirectionalLSTM([w_f, u_f], [w_b, u_b],
-                                            name="bidi_lstm")
+    inputs = sg.input_data(input_tensor, name="input")
+    bidi_lstm = sg.nn.BidirectionalLSTM([w_f, u_f], [w_b, u_b],
+                                        name="bidi_lstm")
     outputs, state_fwd, state_bwd = bidi_lstm(inputs)
     return graph
 
