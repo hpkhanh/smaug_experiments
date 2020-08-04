e31742f6d19f386d4f40a6540bcc20ebb3ce6173
Yuan Yao <yaoyuannnn@gmail.com>
2020-08-04 09:50:56 -0400

    Update Minerva model.

--- models/minerva/minerva_network.py
+++ models/minerva/minerva_network.py
@@ -3,37 +3,31 @@
 """Create the Minerva network."""
 
 import numpy as np
-
-from smaug.core import types_pb2
-from smaug.python.graph import Graph
-from smaug.python.tensor import Tensor
-from smaug.python.ops import data_op
-from smaug.python.ops import nn_ops
+import smaug as sg
 
 def generate_random_data(shape):
   r = np.random.RandomState(1234)
-  return (r.rand(*shape) * 0.005).astype(np.float32)
+  return (r.rand(*shape) * 0.005).astype(np.float16)
 
 def create_minerva_model():
-  with Graph(name="minerva_ref", backend="Reference") as graph:
+  with sg.Graph(name="minerva_smv", backend="SMV") as graph:
     # Tensors and kernels are initialized as NCHW layout.
-    input_tensor = Tensor(
-        data_layout=types_pb2.NHWC, tensor_data=generate_random_data(
-            (1, 28, 28, 1)))
-    fc0_tensor = Tensor(
-        data_layout=types_pb2.NC, tensor_data=generate_random_data((256, 784)))
-    fc1_tensor = Tensor(
-        data_layout=types_pb2.NC, tensor_data=generate_random_data((256, 256)))
-    fc2_tensor = Tensor(
-        data_layout=types_pb2.NC, tensor_data=generate_random_data((256, 256)))
-    fc3_tensor = Tensor(
-        data_layout=types_pb2.NC, tensor_data=generate_random_data((10, 256)))
+    input_tensor = sg.Tensor(
+        data_layout=sg.NHWC, tensor_data=generate_random_data((1, 28, 28, 1)))
+    fc0_tensor = sg.Tensor(
+        data_layout=sg.NC, tensor_data=generate_random_data((256, 784)))
+    fc1_tensor = sg.Tensor(
+        data_layout=sg.NC, tensor_data=generate_random_data((256, 256)))
+    fc2_tensor = sg.Tensor(
+        data_layout=sg.NC, tensor_data=generate_random_data((256, 256)))
+    fc3_tensor = sg.Tensor(
+        data_layout=sg.NC, tensor_data=generate_random_data((10, 256)))
 
-    act = data_op.input_data(input_tensor, "input")
-    act = nn_ops.mat_mul(act, fc0_tensor, activation=types_pb2.ReLU, name="fc0")
-    act = nn_ops.mat_mul(act, fc1_tensor, activation=types_pb2.ReLU, name="fc1")
-    act = nn_ops.mat_mul(act, fc2_tensor, activation=types_pb2.ReLU, name="fc2")
-    act = nn_ops.mat_mul(act, fc3_tensor, name="fc3")
+    act = sg.input_data(input_tensor)
+    act = sg.nn.mat_mul(act, fc0_tensor, activation="relu")
+    act = sg.nn.mat_mul(act, fc1_tensor, activation="relu")
+    act = sg.nn.mat_mul(act, fc2_tensor, activation="relu")
+    act = sg.nn.mat_mul(act, fc3_tensor)
     return graph
 
 if __name__ != "main":
