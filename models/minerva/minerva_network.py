735d6d572993a1402322a814dce03ee537aeb98d
Khanh DISCO Server <hpkhanh@gmail.com>
2022-11-30 20:23:25 +0000

    Add test folder for cifar100-cnn model

--- models/minerva/minerva_network.py
+++ models/minerva/minerva_network.py
@@ -12,16 +12,18 @@ def generate_random_data(shape):
 def create_minerva_model():
   with sg.Graph(name="minerva_smv", backend="SMV") as graph:
     # Tensors and kernels are initialized as NCHW layout.
+    # input_tensor = sg.Tensor(
+    #     data_layout=sg.NHWC, tensor_data=generate_random_data((1, 28, 28, 1)))
     input_tensor = sg.Tensor(
-        data_layout=sg.NHWC, tensor_data=generate_random_data((1, 28, 28, 1)))
+        data_layout=sg.NHWC, tensor_data=np.ones((1, 28, 28, 1), np.float16)*0.005)
     fc0_tensor = sg.Tensor(
-        data_layout=sg.NC, tensor_data=generate_random_data((256, 784)))
+        data_layout=sg.NC, tensor_data=np.ones((256, 784), np.float16)*0.005)
     fc1_tensor = sg.Tensor(
-        data_layout=sg.NC, tensor_data=generate_random_data((256, 256)))
+        data_layout=sg.NC, tensor_data=np.ones((256, 256), np.float16)*0.005)
     fc2_tensor = sg.Tensor(
-        data_layout=sg.NC, tensor_data=generate_random_data((256, 256)))
+        data_layout=sg.NC, tensor_data=np.ones((256, 256), np.float16)*0.005)
     fc3_tensor = sg.Tensor(
-        data_layout=sg.NC, tensor_data=generate_random_data((10, 256)))
+        data_layout=sg.NC, tensor_data=np.ones((10, 256), np.float16)*0.005)
 
     act = sg.input_data(input_tensor)
     act = sg.nn.mat_mul(act, fc0_tensor, activation="relu")
